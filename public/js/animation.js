e55e263804664e5fc028615e896a8e08db812de5
krob79 <emailkyleroberts@gmail.com>
2020-10-06 23:01:09 -0400

    Added Thumbnails

---
+++ public/js/animation.js
@@ -0,0 +1,8 @@
+const { gsap } = require('gsap');
+
+function fadeIn(){
+    console.log("FADING IN");
+    //gsap.to('.grid-x > .cell', {duration: 2, alpha: 0});
+}
+
+module.exports = {fadeIn};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 const { gsap } = require('gsap');
+const {fadeIn} = require('../public/js/animation');
 
 router.use('[\/]',(req, res, next) => {
     console.log("MIDDLEWARE FOR ROOT ONLY");
@@ -9,11 +10,10 @@ router.use('[\/]',(req, res, next) => {
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
-      res.render('index', {projects}, (err, html) => {
-        console.log("I AM AT ROOT sending html");
-        res.send(html);
-      });
+        console.log('RENDERING INDEX');
+      res.render('index', {projects});
 });
+
 
 
 router.get('/about', (req, res, next) => {
@@ -28,10 +28,10 @@ router.get('/projects?/', (req, res, next) => {
 router.get('/projects?/:id', function(req, res, next) {
   const projectId = req.params.id;
   const project = projects.find( ({ id }) => id === +projectId );
-  
+  const thumbs = project.image_urls[1];
   if (project) {
     console.log(`Going to project ${projectId}...`);
-    return res.render('project', { project });
+    return res.render('project', { project, thumbs });
   } else {
     const err = new Error("This project doesn't exist!");
     res.render('error', {message: err.message});
