151975fc27f99df4fd1df8b5bb11e93171e19f9e
krob79 <emailkyleroberts@gmail.com>
2020-10-22 17:22:28 -0400

    Added new icon, favicon.ico and Express router for the favicon request

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,21 @@ const { projects } = require('../data.json');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
       res.render('index', {projects});
 });
 
-router.get('/about', (req, res, next) => {
+router.get('/about', (req, res) => {
     res.render('about');
 });
 
 //redirects to root if project ID is not specified
-router.get('/projects?/', (req, res, next) => {
+router.get('/projects?/', (req, res) => {
     res.redirect('/');
 });
 
 /* GET projects page. RegEx (?) added to check for 'project' or 'projects' */
-router.get('/projects?/:id', function(req, res, next) {
+router.get('/projects?/:id', function(req, res) {
   const projectId = req.params.id;
   const project = projects.find( ({ id }) => id === +projectId );
 
@@ -33,12 +33,17 @@ router.get('/projects?/:id', function(req, res, next) {
   }
 });
 
-router.get('/:page', function(req, res, next) {
-    const pageName = req.params.page;
-    const err = new Error();
-    err.status = 404;
-    console.log(`ERROR: ${err.status}. It looks like the page you typed in (${pageName}) doesn't exist.`);
-    res.render('page-not-found', {message: `${err.status}. It looks like the page you typed in (${pageName}) doesn't exist.`});
+router.get('/favicon.ico', function(req, res) {
+    res.sendFile("/static/favicon.io");
+});
+
+router.get('/:page', function(req, res) {
+  const pageName = req.params.page;
+  const err = new Error();
+  err.status = 404;
+  console.log(`Hey, ERROR: ${err.status}. It looks like the page you typed in (${pageName}) doesn't exist.`);
+  res.render('page-not-found', {message: `${err.status}. It looks like the page you typed in (${pageName}) doesn't exist.`});
+  
   });
 
 module.exports = router;
